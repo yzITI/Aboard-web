8086fd2ab8a63838ec9d3198d13cefaef2e07e51
CutePikachu <z5191511@ad.unsw.edu.au>
2022-06-20 21:02:35 +1000

    add home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
-<script setup>
-let cot = $ref(0)
-
-function inc () {
-  cot++
-}
-</script>
-
 <template>
-  <h1>Home</h1>
-  <p>cot: {{ cot }}</p>
-  <button @click="inc">Click Me</button>
+  <div class="flex flex-col justify-center items-center h-screen text-slate-800 bg-slate-200">
+    <h1 style="margin-bottom: 50px; font-size: 3rem;">Aboard</h1>
+     <span class="flex">
+        <input class="border rounded-lg p-3 m-1" style="min-width: 300px" placeholder="Enter your name">
+        <img :src="dice" class="w-12 m-3"/>
+      </span>
+  </div>
 </template>
+
+<script setup>
+import dice from '../utils/dice.svg'
+</script>
